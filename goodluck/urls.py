fdd975da9eb1414093c387bea918aee1bad86c38
shameem-0077 <shameemoff52@gmail.com>
2023-10-13 14:02:08 +0530

    added: added new domain
    Changes to be committed:
    	modified:   goodluck/__pycache__/settings.cpython-38.pyc
    	modified:   goodluck/settings.py
    	modified:   goodluck/urls.py

--- goodluck/urls.py
+++ goodluck/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('good-luck-admin/', admin.site.urls),
     path('', include("web.urls", namespace="web_urls")),
 ]
 
